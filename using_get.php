0540b3951984304218c9891cb5eef0a5da10fdf8
Temi Michael <olasehindemichael285@gmail.com>
2023-09-15 13:22:47 +0100

    File name change

--- using_get.php
+++ using_get.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="new1.php" method="get">
+    <form action="using_get.php" method="get">
         Name : <input type="text" name="name" id=""> <br>
         <input type="submit" value="Submit">
 
