b269b33d741d6d7347cb7fe620f08d1af66e8421
Temi Michael <olasehindemichael285@gmail.com>
2023-09-16 02:54:00 +0100

    Loops

---
+++ while.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="while.php" method="post">
+
+
+
+
+
+    </form> <br>
+
+    <?php 
+    
+    $index = 1;
+    while ($index <= 10) {
+        echo "$index <br>";
+        $index++;
+    }
+    
+    
+    
+    ?>
+</body>
+</html>
