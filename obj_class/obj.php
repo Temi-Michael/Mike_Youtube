69f56f35a1fdb2678fa02fde34d8e5f59f67bbcb
Temi Michael <olasehindemichael285@gmail.com>
2023-09-17 01:12:39 +0100

    Class, Objects and Constructors

--- obj_class/obj.php
+++ obj_class/obj.php
@@ -26,13 +26,17 @@
 
 
     $book1 = new Book("Harry Porter", "J.K.Rowling", 400);
-
     $book2 = new Book("Lord Of The Rings", "Tolkien", 700);
+    $book3 = new Book("Despicable Me", "Sony Pictures", 300);
+    $book4 = new Book("Frozen", "Waltz Disney", 500);
+    $book5 = new Book("Star Wars", "Warner Bros", 2500);
 
 
     echo $book1 -> title. "<br>";
-    echo $book2 -> author;
-
+    echo $book2 -> author. "<br>";
+    echo $book3 -> pages. "<br>";
+    echo $book4 -> title. "<br>";
+    echo $book5 -> pages. "<br>";
     
     ?>
 
